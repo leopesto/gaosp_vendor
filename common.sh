c25c8248bc9602b75a0296987b29f1a416d04284
leopesto <leo94leo@hotmail.com>
2011-12-14 12:29:17 +0100

    Update common.sh

--- common.sh
+++ common.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-(cd qcom/android-open/libopencorehw && ./$1.sh)
-(cd qcom/android-open/loc_api && ./$1.sh)
+#(cd qcom/android-open/libopencorehw && ./$1.sh)
+(cd qcom/proprietary-open/loc_api && ./$1.sh)
 (cd qcom/proprietary-open/dsp/libqcamera && ./$1.sh)
 (cd qcom/proprietary-open/dsp/mm && ./$1.sh)
 (cd qcom/proprietary-open/libaudioeq && ./$1.sh)
 (cd qcom/proprietary-open/libgps && ./$1.sh)
 (cd qcom/proprietary-open/libhgl && ./$1.sh)
+(cd qcom/proprietary-open/libegl && ./$1.sh)
